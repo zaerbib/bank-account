eed587f024f2667526226434b5ac273c72bc38f4
zaerbib <ilunga.joel@yahoo.fr>
2025-01-16 22:33:31 +0100

    init project

---
+++ src/main/java/com/bank/account/kata/bank_account/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.bank.account.kata.bank_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/account/kata/bank_account/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.account.kata.bank_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
